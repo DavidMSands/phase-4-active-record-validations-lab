483539050779b27950c9c3e73d3f6a392bcc4b6b
David <Davidmaxsands@gmail.com>
2022-03-16 01:21:11 -0600

    commit tree aa7752d653a9438fc45e01ad402d8d68a1fb04f0

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,21 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: ['Fiction', 'Non-Fiction']
+    validate :title_must_be_click_baity
+
+    REGEX_PATTERNS = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+    ]
+
+
+    def title_must_be_click_baity
+        if REGEX_PATTERNS.none? { |p| p.match title }
+            errors.add(:title, "must be clickbait-y")
+        end
+    end
 end
